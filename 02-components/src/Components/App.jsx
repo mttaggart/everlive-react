132423e1f57c35950f423fcdb607e66101b5855a
mttaggart <michael.t.taggart@gmail.com>
2019-07-05 10:57:37 -0700

    Prepare for 02

--- 02-components/src/Components/App.jsx
+++ 02-components/src/Components/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import HeroList from "../Components/Heroes/HeroList";
-import heroes from "../heroes";
+import Header from "./Layout/Header";
+import HeroList from "./Heroes/HeroList";
+import heroes from "../lib/heroes";
 
 const App = () => (
     <div>
-        <header>
+        <Header>
             <h1>The Paragon Group</h1>
             <h2>The heroes you're looking for</h2>
-        </header>
+        </Header>
         <HeroList heroes={heroes}/>
     </div>
-)
+);
 
 export default App;
--- 02-components/src/Components/Heroes/HeroList.jsx
+++ 02-components/src/Components/Heroes/HeroList.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import HeroCard from "./HeroCard";
 
+const style = {
+    background: "navy",
+    color: "white",
+    fontFamily: "sans-serif"
+}
+
 const HeroList = ({heroes}) => {
     const heroCards = heroes.map((h,idx) => (
         <HeroCard hero={h} key={idx} />
     ));
     return (
-        <div>
+        <div style={style}>
             {heroCards}
         </div>
     )
